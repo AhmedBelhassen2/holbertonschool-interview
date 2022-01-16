f124f8ddf80a42c5a22446bb85806e6e47c941e1
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-01-16 19:46:04 +0100

    all

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,31 +1,43 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * 
- * 
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: pointer to head of list.
+ * @number: the number to add.
+ * Return: the address of the new node, or NULL if it failed.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    
-    listint_t *arg;
-    if (!head)
+	listint_t *node, *iter;
+	
+	if (!head)
+	return NULL;
+	iter = *head;
+
+	node = malloc(sizeof(listint_t));
+	if (!node)
+	return NULL;
+	node->n = number;
+	node->next = NULL;
+
+	if (!*head)
 	{
-        return (NULL);
-    }
-    arg = malloc(sizeof(listint_t));
-    if (arg == NULL)
-    {
-        return (NULL);
-    }
-    arg->n = number;
-	arg->next = NULL;
-    while((*head)->n < number)
-    {
-        head = &((*head)->next);
-    }
-    arg->next = (*head);
-   (*head) = arg;
-    return (arg);
+		*head = node;
+		return node;
+	}
+
+	if (iter->n > number)
+	{
+		*head = node;
+		node->next = iter;
+		return node;
+	}
+
+	while(iter->next && iter->next->n < number)
+	iter = iter->next;
+	node->next = iter->next;
+	iter->next = node;
+	return node;
 }
