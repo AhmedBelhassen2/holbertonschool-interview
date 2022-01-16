f4cf12b0e2fddf8e698a5d0a4d55217ea2db7470
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-01-16 20:53:54 +0100

    all

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,115 +1,112 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "sandpiles.h"
-static void print_grid(int grid[3][3]);
 
 /**
- * sandpiles_sum - Sums 2 grids, topples the "sandpile" and prints a resulting
- *                 stable "sandpile" at the end.
- *      Args:
- *     @grid1:   - Left 3x3 int grid.
- *     @grid2:   - Right 3x3 int grid.
- */
-
-
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
-{
-	int i = 0, j = 0;
-
-	printf("=\n");
-	while (stable(grid1) != 0)
-	{
-		for (i = 0; i <= 3; i++)
-			for (j = 0; j <= 3; j++)
-				grid1[i][j] += grid2[i][j];
-
-		print_grid(grid1);
-		topple(grid1, grid2);
-		print_grid(grid1);
-
-	}
-	printf("=\n");
-}
-
-/**
- * topple - "Shakes" the grid to make the sand move one step and calculates the
- *          new transform matrix for next step.
- *   Args:
- *  @grid1: 3x3 int grid.
- *  @grid2: 3x3 int grid.
- *
- *  Return: The new transform matrix calculated.
- */
-
-
-void topple(int grid[3][3], int newGrid[3][3])
-{
-	int i = 0, j = 0, size = 3, minHeight = 3;
-
-	/* for (i = 0; i <= size; i++) */
-	/* 	for (j = 0; j <= size; j++) */
-	/* 		newGrid[i][j] = 0; */
-
-	for (i = 0; i <= size; i++)
-		for (j = 0; j <= size; j++)
-			if (grid[i][j] <= minHeight)
-				newGrid[i][j] = grid[i][j];
-
-	for (i = 0; i <= size; i++)
-		for (j = 0; j <= size; j++)
-		{
-			if (grid[i][j] > minHeight)
-			{
-				newGrid[i][j] += grid[i][j] - 4;
-
-				if (i + 1 < size)
-					newGrid[i + 1][j]++;
-				if (i - 1 >= 0)
-					newGrid[i - 1][j]++;
-				if (j + 1 < size)
-					newGrid[i][j + 1]++;
-				if (j - 1 >= 0)
-					newGrid[i][j - 1]++;
-			}
-		}
-
-	for (i = 0; i <= size; i++)
-		for (j = 0; j <= size; j++)
-			grid[i][j] = newGrid[i][j];
-}
-
-/**
- * stable - Checks if the 3x3 grid is stable
- * @grid: 3x3 int grid.
- * Return: 0 if stable(all numbers less than 3)
- *         1 if not.
- */
-int stable(int grid[3][3])
-{
-	int i = 0, j = 0;
-
-	for (i = 0; i < 3; i++)
-		for (j = 0; j < 3; j++)
-			if (grid[i][j] > 3)
-				return(1);
-	return(0);
-}
-
-/**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- *
+ * print_grid - print a grid
+ * @grid: grid
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+printf(" ");
+printf("%d", grid[i][j]);
+}
+printf("\n");
+}
+}
+
+/**
+ * sandpiles_sum - sums two sandpiles
+ * @grid1: first grid
+ * @grid2: second grid
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+int i, j;
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+grid1[i][j] += grid2[i][j];
+}
+while (!is_stable(grid1))
+{
+printf("=\n");
+print_grid(grid1);
+topple(grid1);
+}
+}
+
+/**
+ * topple - topples sand
+ * @grid: sandpile
+ */
+void topple(int grid[3][3])
+{
+int i, j;
+int new_gird[3][3];
+copy_grid(grid, new_gird);
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (new_gird[i][j] > 3)
+{
+grid[i][j] -= 4;
+if (i + 1 < 3)
+grid[i + 1][j] += 1;
+if (j + 1 < 3)
+grid[i][j + 1] += 1;
+if (i - 1 >= 0)
+grid[i - 1][j] += 1;
+if (j - 1 >= 0)
+grid[i][j - 1] += 1;
+}
+}
+}
+}
+
+/**
+ * is_stable - checks if sandpile is stable
+ *
+ * @grid: sandpile
+ * Return: 1 if stable 0 if not
+ */
+int is_stable(int grid[3][3])
+{
+int i, j;
+
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (grid[i][j] > 3)
+return (0);
+}
+}
+return (1);
+}
+
+/**
+ * copy_grid - copies a grid
+ *
+ * @grid: grid to copy
+ * @new_grid: destination grid
+ * Return: a copy of the grid
+ */
+void copy_grid(int grid[3][3], int new_grid[3][3])
+{
+int i, j;
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+new_grid[i][j] = grid[i][j];
+}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,9 +5,8 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-
-int stable(int grid[3][3]);
-
-void topple(int grid1[3][3], int grid2[3][3]);
+void topple(int grid[3][3]);
+int is_stable(int grid[3][3]);
+void copy_grid(int grid[3][3], int new_grid[3][3]);
 
 #endif
