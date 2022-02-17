865be5c71a4c05dbbb006865671b2e226d1eaee0
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-02-17 17:13:10 +0100

    0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-""" determines if a given data set represents a valid UTF-8 encoding. """
+"""
+0. UTF-8 Validation
+"""
+
 
 def validUTF8(data):
     """
-    UTF-8 Encode
+    0. UTF-8 Validation
+    Return: True if data is a valid UTF-8 encoding, else return False
+    - A character in UTF-8 can be 1 to 4 bytes long
+    - The data set can contain multiple characters
+    - The data will be represented by a list of integers
+    - Each integer represents 1 byte of data, therefore you only need to
+    handle the 8 least significant bits of each integer
     """
+
     successive_10 = 0
-    for nb_b in data:
+    for b in data:
         # b = bin(b).replace('0b', '').rjust(8, '0')[-8:]
-        nb_b = format(nb_b, '#010b')[-8:]
+        b = format(b, '#010b')[-8:]
         if successive_10 != 0:
             successive_10 -= 1
-            if not nb_b.startswith('10'):
+            if not b.startswith('10'):
                 return False
-        elif nb_b[0] == '1':
-            successive_10 = len(nb_b.split('0')[0])
+        elif b[0] == '1':
+            successive_10 = len(b.split('0')[0])
             if successive_10 == 1 or successive_10 > 4:
                 return False
             successive_10 -= 1
 
     return True if successive_10 == 0 else False
-
---
+++ 0x09-utf8_validation/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" determines if a given data set represents a valid UTF-8 encoding. """
+
+def validUTF8(data):
+    """
+    UTF-8 Encode
+    """
+    
+    successive_10 = 0
+    for nb_b in data:
+        # b = bin(b).replace('0b', '').rjust(8, '0')[-8:]
+        nb_b = format(nb_b, '#010b')[-8:]
+        if successive_10 != 0:
+            successive_10 -= 1
+            if not nb_b.startswith('10'):
+                return False
+        elif nb_b[0] == '1':
+            successive_10 = len(nb_b.split('0')[0])
+            if successive_10 == 1 or successive_10 > 4:
+                return False
+            successive_10 -= 1
+
+    return True if successive_10 == 0 else False
+
