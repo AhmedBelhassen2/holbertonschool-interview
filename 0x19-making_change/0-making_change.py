5fa119749b35f838aec94319a62b7d2c1d5a7c76
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-10-27 17:01:58 +0100

    sort

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 0. Change comes from within """
+
+
+def makeChange(coins, total):
+    """ Given a pile of coins of different values, determine the fewest number
+        of coins needed to meet a given amount total.
+    """
+    
+    if total <= 0:
+        return 0
+    if total in coins:
+        return 1
+    if len(coins) <= 0:
+        return -1
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for s in range(1, total + 1):
+        for c in coins:
+            if s - c >= 0:
+                dp[s] = min(dp[s], 1 + dp[s - c])
+    return dp[total] if dp[total] != total + 1 else -1
+        
