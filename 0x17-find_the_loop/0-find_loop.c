b30859ebcd0133daa65d7cbe3225e95597ec8457
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2023-02-12 18:13:25 +0100

    0-find_loop.c

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,78 +1,28 @@
-// C# program to detect loop in a linked list
-using System;
-using System.Collections.Generic;
+#include "lists.h"
 
-class LinkedList {
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: pointer to the head of the linked list
+ * Return: the address of the loop node
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *start = head, *coyote = head;
 
-	// head of list
-	public Node head;
-
-	/* Linked list Node*/
-	public class Node {
-		public int data;
-		public Node next;
-		public Node(int d)
+	while (coyote && coyote->next)
+	{
+		coyote = coyote->next->next;
+		start = start->next;
+		if (coyote == start)
 		{
-			data = d;
-			next = null;
+			start = head;
+			while (start != coyote)
+			{
+				start = start->next;
+				coyote = coyote->next;
+			}
+			return (coyote);
 		}
 	}
-
-	/* Inserts a new Node at front of the list. */
-	public void push(int new_data)
-	{
-		/* 1 & 2: Allocate the Node &
-				Put in the data*/
-		Node new_node = new Node(new_data);
-
-		/* 3. Make next of new Node as head */
-		new_node.next = head;
-
-		/* 4. Move the head to point to new Node */
-		head = new_node;
-	}
-
-	// Returns true if there is a loop in linked
-	// list else returns false.
-	public static bool detectLoop(Node h)
-	{
-		HashSet<Node> s = new HashSet<Node>();
-		while (h != null) {
-			// If we have already has this node
-			// in hashmap it means their is a cycle
-			// (Because you we encountering the
-			// node second time).
-			if (s.Contains(h))
-				return true;
-
-			// If we are seeing the node for
-			// the first time, insert it in hash
-			s.Add(h);
-
-			h = h.next;
-		}
-
-		return false;
-	}
-
-	/* Driver code*/
-	public static void Main(String[] args)
-	{
-		LinkedList llist = new LinkedList();
-
-		llist.push(20);
-		llist.push(4);
-		llist.push(15);
-		llist.push(10);
-
-		/*Create loop for testing */
-		llist.head.next.next.next.next = llist.head;
-
-		if (detectLoop(llist.head))
-			Console.WriteLine("Loop Found");
-		else
-			Console.WriteLine("No Loop");
-	}
+	return (NULL);
 }
-
-// This code has been contributed by 29AjayKumar
